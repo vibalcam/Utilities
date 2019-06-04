a1860dc92e21668b85891c428879f79343528545
vibalcam <49537743+vibalcam@users.noreply.github.com>
2019-06-04 19:30:38 +0200

    CBM Widget

---
+++ app/src/main/java/com/utilities/vibal/utilities/widget/CashBoxWidget.java
@@ -0,0 +1,62 @@
+package com.utilities.vibal.utilities.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.utilities.vibal.utilities.R;
+import com.utilities.vibal.utilities.ui.cashBoxItem.CashBoxItemActivity;
+import com.utilities.vibal.utilities.ui.cashBoxManager.CashBoxManagerActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class CashBoxWidget extends AppWidgetProvider {
+    public static final int ADD_REQUEST = 1;
+    public static final int DETAILS_REQUEST = 2;
+    public static final String ACTION_REFRESH = "com.utilities.vibal.utilities.widget.actionRefresh";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.cash_box_manager_widget);
+
+            // OnClick for button
+            Intent buttonIntent = new Intent(context, CashBoxManagerActivity.class);
+            buttonIntent.putExtra(CashBoxManagerActivity.EXTRA_ACTION,CashBoxManagerActivity.ACTION_ADD_CASHBOX);
+            views.setOnClickPendingIntent(R.id.addButtonCBMWidget,
+                    PendingIntent.getActivity(context,ADD_REQUEST,buttonIntent,0));
+
+            // Set-up ListView
+            Intent serviceIntent = new Intent(context, CashBoxWidgetService.class);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+            views.setRemoteAdapter(R.id.listCBMWidget,serviceIntent);
+            views.setEmptyView(R.id.listCBMWidget,R.id.listEmptyCBMWidget);
+
+            // OnClick for ListView
+            Intent listIntent = new Intent(context, CashBoxItemActivity.class);
+            views.setPendingIntentTemplate(R.id.listCBMWidget,
+                    PendingIntent.getActivity(context,DETAILS_REQUEST,listIntent,0));
+
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        if(ACTION_REFRESH.equals(intent.getAction())) {
+//            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,AppWidgetManager.INVALID_APPWIDGET_ID);
+//            AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(appWidgetId, R.id.listCBMWidget);
+//        } else
+//            super.onReceive(context, intent);
+//    }
+}
+
---
+++ app/src/main/java/com/utilities/vibal/utilities/widget/CashBoxWidgetService.java
@@ -0,0 +1,91 @@
+package com.utilities.vibal.utilities.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.utilities.vibal.utilities.R;
+import com.utilities.vibal.utilities.io.IOCashBoxManager;
+import com.utilities.vibal.utilities.models.CashBox;
+import com.utilities.vibal.utilities.models.CashBoxManager;
+import com.utilities.vibal.utilities.ui.cashBoxItem.CashBoxItemActivity;
+
+import java.text.NumberFormat;
+
+public class CashBoxWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new CashBoxWidgetItemFactory(getApplicationContext());
+    }
+
+    class CashBoxWidgetItemFactory implements RemoteViewsFactory {
+        private Context context;
+        private CashBoxManager cashBoxManager;
+        private NumberFormat currencyFormat;
+
+        CashBoxWidgetItemFactory(Context context) {
+            this.context = context;
+            cashBoxManager = new CashBoxManager();
+            currencyFormat = NumberFormat.getCurrencyInstance();
+        }
+
+        @Override
+        public void onCreate() {}
+
+        @Override
+        public void onDataSetChanged() {
+            cashBoxManager = IOCashBoxManager.loadCashBoxManager(context);
+            currencyFormat = NumberFormat.getCurrencyInstance();
+        }
+
+        @Override
+        public void onDestroy() {}
+
+        @Override
+        public int getCount() {
+            return cashBoxManager.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            // Set up layout
+            CashBox cashBox = cashBoxManager.get(position);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.item_cbm_widget);
+            views.setTextViewText(R.id.nameCBMWidgetItem,cashBox.getName());
+            views.setTextViewText(R.id.amountCBMWidgetItem,currencyFormat.format(cashBox.getCash()));
+            if(cashBox.getCash()<0)
+                views.setTextColor(R.id.amountCBMWidgetItem,context.getColor(R.color.colorNegativeNumber));
+            else
+                views.setTextColor(R.id.amountCBMWidgetItem,context.getColor(R.color.colorPositiveNumber));
+
+            // Intent for OnClick
+            Intent fillIntent = new Intent();
+            fillIntent.putExtra(CashBoxItemActivity.EXTRA_INDEX,position);
+            fillIntent.putExtra(CashBoxItemActivity.EXTRA_CASHBOX_MANAGER, (Parcelable) cashBoxManager);
+            views.setOnClickFillInIntent(R.id.listItem_cbmWidget,fillIntent);
+            return views;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
